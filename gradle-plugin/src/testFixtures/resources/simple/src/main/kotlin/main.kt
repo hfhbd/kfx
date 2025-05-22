fa7cfb34667d034a86cbf6440162f129cb00c859
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-05-22 16:18:01 +0200

    Fix test

--- gradle-plugin/src/testFixtures/resources/simple/src/main/kotlin/main.kt
+++ gradle-plugin/src/testFixtures/resources/simple/src/main/kotlin/main.kt
@@ -1,6 +1,15 @@
 import client.foo
+import client.fooCSRF
+import io.github.hfhbd.Foo
+import io.github.hfhbd.FooCsv
 import io.ktor.client.HttpClient
 
 suspend fun main() {
-    HttpClient().foo()
+    val client = HttpClient()
+    client.fooCSRF()
+    val csv: List<FooCsv> = client.foo(
+        input = listOf(Foo(foo = "foo")),
+        X_CSRF_Token = "someToken",
+    )
+    val s = csv.single().FOO
 }
