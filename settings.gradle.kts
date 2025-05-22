2a2bcbf665f931fa0303a8fca3ea40fce4be8677
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-05-22 18:05:17 +0200

    Enable BuildScans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,19 @@ pluginManagement {
 plugins {
     id("myRepos")
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.gradle.develocity") version "4.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/terms-of-service")
+        termsOfUseAgree.set("yes")
+        val isCI = providers.environmentVariable("CI").isPresent
+        publishing {
+            onlyIf { isCI }
+        }
+        tag("CI")
+    }
 }
 
 rootProject.name = "kfx"
