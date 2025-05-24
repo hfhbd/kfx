9d61040c5ef6cd6568e3142e25c63aae18d62400
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-05-24 20:30:39 +0200

    Add WASI target if possible

--- ktor-server-soap-plugin/build.gradle.kts
+++ ktor-server-soap-plugin/build.gradle.kts
@@ -9,21 +9,6 @@ kotlin.sourceSets {
             api(libs.serialization.xml)
         }
     }
-    jvmTest {
-        dependencies {
-            implementation(libs.ktor.server.test.host)
-            implementation(libs.ktor.server.content.negotiation)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.serialization.kotlinx.xml)
-            implementation(libs.ktor.server.double.receive)
-           // implementation(testFixtures(projects.wsdlFir))
-
-           // implementation(testFixtures(projects.core))
-            implementation(libs.serialization.json)
-            implementation(projects.wsdlModel)
-            implementation(libs.ktor.client.cio)
-        }
-    }
 }
 
 licensee {
