7ad80de3d5bd934df70d55d3839db8cff78e6d20
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-09-07 19:47:16 +0200

    Fix member

---
+++ kotlin-poet/src/test/kotlin/CallUuidTest.kt
@@ -0,0 +1,35 @@
+import com.squareup.kotlinpoet.NameAllocator
+import io.github.hfhbd.kfx.codegen.CodeGenTree
+import io.github.hfhbd.kfx.codegen.CodeGenTree.Expression.CallFunction
+import io.github.hfhbd.kfx.codegen.CodeGenTree.Expression.CallStatic
+import io.github.hfhbd.kfx.codegen.CodeGenTree.Expression.Chain
+import io.github.hfhbd.kfx.toCodeBlock
+import io.github.hfhbd.kfx.toPoetType
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CallUuidTest {
+    @Test
+    fun call() {
+        val uuid = CodeGenTree.NormalClass("kotlin.uuid", listOf("Uuid"))
+
+        assertEquals(
+            """kotlin.uuid.Uuid.random()""",
+            Chain(
+                lhs = CallStatic(
+                    uuid,
+                ),
+                rhs = CallFunction(
+                    function = CodeGenTree.Function(
+                        name = "random",
+                        emptyList(),
+                        uuid,
+                    ),
+                    parameters = emptyList(),
+                ),
+            ).toCodeBlock(nameAllocator = NameAllocator()) {
+                toPoetType(false)
+            }.toString(),
+        )
+    }
+}
