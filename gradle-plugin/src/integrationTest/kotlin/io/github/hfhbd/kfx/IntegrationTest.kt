0f4d295d798d42d2d8d7b0357c2cb6a524c4c8fd
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-09-16 00:15:13 +0200

    Remove software types plugin (#95)

--- gradle-plugin/src/integrationTest/kotlin/io/github/hfhbd/kfx/IntegrationTest.kt
+++ gradle-plugin/src/integrationTest/kotlin/io/github/hfhbd/kfx/IntegrationTest.kt
@@ -15,12 +15,6 @@ class IntegrationTest {
         build(projectDir.toFile(), ":assemble")
     }
 
-    @Test
-    fun softwareTypes() {
-        val projectDir = fixtureDir / "resources" / "software-types"
-        build(projectDir.toFile(), ":assemble")
-    }
-
     private fun build(projectDir: File, vararg tasks: String): BuildResult {
         return GradleRunner.create()
             .withProjectDir(projectDir)
--- gradle-plugin/src/main/kotlin/io/github/hfhbd/kfx/KfxPlugin.kt
+++ gradle-plugin/src/main/kotlin/io/github/hfhbd/kfx/KfxPlugin.kt
@@ -2,12 +2,8 @@ package io.github.hfhbd.kfx
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.internal.plugins.software.SoftwareType
 
 abstract class KfxPlugin : Plugin<Project> {
-    @get:SoftwareType(name = "kfx", disableModelManagement = true)
-    abstract val kfx: KfxExtension
-
     override fun apply(target: Project) {
         target.extensions.create(KfxExtension::class.java, "kfx", KfxExtensionImpl::class.java)
     }
