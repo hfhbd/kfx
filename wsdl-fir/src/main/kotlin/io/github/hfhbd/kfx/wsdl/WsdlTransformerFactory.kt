b40f36bb6c028c52b51ab7ad9596bb566e532617
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-10-05 18:50:43 +0200

    WIP

--- wsdl-fir/src/main/kotlin/io/github/hfhbd/kfx/wsdl/WsdlTransformerFactory.kt
+++ wsdl-fir/src/main/kotlin/io/github/hfhbd/kfx/wsdl/WsdlTransformerFactory.kt
@@ -3,6 +3,6 @@ package io.github.hfhbd.kfx.wsdl
 import kotlinx.serialization.modules.SerializersModule
 
 interface WsdlTransformerFactory {
-    fun serializerModule(): SerializersModule
     fun create(): WsdlTransformer
+    fun serializerModule(): SerializersModule
 }
