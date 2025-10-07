d9674b4287ce28415c6613b28bd0a8869c0f213f
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-10-07 13:30:38 +0200

    Fix test

--- xsd-fir/src/gradleDependencyVerification/kotlin/org/gradle/schema/dependency_verification/Md5.kt
+++ xsd-fir/src/gradleDependencyVerification/kotlin/org/gradle/schema/dependency_verification/Md5.kt
@@ -1,7 +1,9 @@
 package org.gradle.schema.dependency_verification
 
+import kotlin.String
 import kotlin.collections.List
 import kotlin.collections.emptyList
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
@@ -18,7 +20,10 @@ public data class Md5(
     namespace = "https://schema.gradle.org/dependency-verification",
   )
   public val `also-trust`: List<AlsoTrust> = emptyList(),
-  public val value: String,
+  @SerialName(value = "value")
+  public val `value`: String,
+  @SerialName(value = "origin")
   public val origin: String? = null,
+  @SerialName(value = "reason")
   public val reason: String? = null,
 )
