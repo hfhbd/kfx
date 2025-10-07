a639c6d705fed7c770d4e045acfb236598e1a296
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-10-07 14:28:35 +0200

    Fix test

--- xsd-fir/src/gradleDependencyVerification/kotlin/org/gradle/schema/dependency_verification/Trusting.kt
+++ xsd-fir/src/gradleDependencyVerification/kotlin/org/gradle/schema/dependency_verification/Trusting.kt
@@ -1,5 +1,8 @@
 package org.gradle.schema.dependency_verification
 
+import kotlin.Boolean
+import kotlin.String
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 
@@ -9,9 +12,14 @@ import nl.adaptivity.xmlutil.serialization.XmlSerialName
   namespace = "https://schema.gradle.org/dependency-verification",
 )
 public data class Trusting(
-  override val group: String? = null,
-  override val name: String? = null,
-  override val version: String? = null,
-  override val regex: Boolean? = null,
-  override val file: String? = null,
-) : Coordinates
+  @SerialName(value = "group")
+  public val group: String? = null,
+  @SerialName(value = "name")
+  public val name: String? = null,
+  @SerialName(value = "version")
+  public val version: String? = null,
+  @SerialName(value = "regex")
+  public val regex: Boolean? = null,
+  @SerialName(value = "file")
+  public val `file`: String? = null,
+)
