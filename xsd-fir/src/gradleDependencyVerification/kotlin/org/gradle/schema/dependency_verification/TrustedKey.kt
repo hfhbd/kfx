8bb58693ccec1f9fd792e0ec1ae55aa6528ec7d3
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-10-07 13:28:03 +0200

    Fix test

--- xsd-fir/src/gradleDependencyVerification/kotlin/org/gradle/schema/dependency_verification/TrustedKey.kt
+++ xsd-fir/src/gradleDependencyVerification/kotlin/org/gradle/schema/dependency_verification/TrustedKey.kt
@@ -1,7 +1,9 @@
 package org.gradle.schema.dependency_verification
 
+import kotlin.String
 import kotlin.collections.List
 import kotlin.collections.emptyList
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
@@ -18,10 +20,16 @@ public data class TrustedKey(
     namespace = "https://schema.gradle.org/dependency-verification",
   )
   public val trusting: List<Trusting> = emptyList(),
-  val id: String,
-  val group: String? = null,
-  val name: String? = null,
-  val version: String? = null,
-  val file: String? = null,
-  val regex: String? = null,
+  @SerialName(value = "id")
+  public val id: String,
+  @SerialName(value = "group")
+  public val group: String? = null,
+  @SerialName(value = "name")
+  public val name: String? = null,
+  @SerialName(value = "version")
+  public val version: String? = null,
+  @SerialName(value = "file")
+  public val `file`: String? = null,
+  @SerialName(value = "regex")
+  public val regex: String? = null,
 )
