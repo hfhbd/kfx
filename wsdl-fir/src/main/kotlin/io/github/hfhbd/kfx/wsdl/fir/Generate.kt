33d013af2e4ca66774fbe2802a1c2efd50599678
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-11-16 15:06:27 +0100

    Use soapAction from actual binding

--- wsdl-fir/src/main/kotlin/io/github/hfhbd/kfx/wsdl/fir/Generate.kt
+++ wsdl-fir/src/main/kotlin/io/github/hfhbd/kfx/wsdl/fir/Generate.kt
@@ -121,7 +121,6 @@ private fun WSDL.toIr(
         }
     }.toSet()
 
-
     val classes = irTypes.resolveMembers(faults)
     val operations = mutableSetOf<IRTree.Operation>()
 
@@ -667,25 +666,25 @@ private fun List<Element>.mapToIr(
         val elementName = (it.name ?: it.ref!!.split(":")[1])
 
         elementName.replaceFirstChar { it.lowercaseChar() } to
-                IRTree.Member(
-                    type = if (it.maxOccurs == "unbounded") {
-                        IRTree.Type.LIST(type)
-                    } else {
-                        type
-                    },
-                    nullable = it.nillable == true || it.minOccurs == "0",
-                    serialName = elementName,
-                    namespace = if (it.ref == null) {
-                        schema.targetNamespace
-                    } else {
-                        ns?.let { getNamespace(it) }
-                    },
-                    documentation = it.annotation?.documentation(),
-                    xmlType = IRTree.XmlType.Element,
-                    requirements = emptyList(),
-                    isOverride = false,
-                    deprecated = false,
-                )
+            IRTree.Member(
+                type = if (it.maxOccurs == "unbounded") {
+                    IRTree.Type.LIST(type)
+                } else {
+                    type
+                },
+                nullable = it.nillable == true || it.minOccurs == "0",
+                serialName = elementName,
+                namespace = if (it.ref == null) {
+                    schema.targetNamespace
+                } else {
+                    ns?.let { getNamespace(it) }
+                },
+                documentation = it.annotation?.documentation(),
+                xmlType = IRTree.XmlType.Element,
+                requirements = emptyList(),
+                isOverride = false,
+                deprecated = false,
+            )
     }
 }
 
