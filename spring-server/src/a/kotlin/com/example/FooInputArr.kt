3e7c2b3fa2530ce0f90abf130af84e48fd3236a7
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-12-02 16:47:17 +0100

    Fix nested lists

--- spring-server/src/a/kotlin/com/example/FooInputArr.kt
+++ spring-server/src/a/kotlin/com/example/FooInputArr.kt
@@ -7,5 +7,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 public data class FooInputArr(
   public val byBar: List<FooInputArrByBar>,
-  public val byBaz: List<FooInputArrByBaz> = emptyList(),
+  public val byBaz: List<List<FooInputArrByBaz>> = emptyList(),
 )
