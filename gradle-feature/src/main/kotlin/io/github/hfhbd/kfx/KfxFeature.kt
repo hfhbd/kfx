a7a0a8065fb5eff32b56be2d26e5f0409fda6310
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2026-06-21 14:24:14 +0200

    DCL: Add MainFeature for the Kotlin ecosystem (#280)
    * Bump kotlin from 2.3.21 to 2.4.0
    
    Bumps `kotlin` from 2.3.21 to 2.4.0.
    
    Updates `org.jetbrains.kotlin:kotlin-gradle-plugin-api` from 2.3.21 to 2.4.0
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.21...v2.4.0)
    
    Updates `org.jetbrains.kotlin:kotlin-gradle-plugin` from 2.3.21 to 2.4.0
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.21...v2.4.0)
    
    Updates `org.jetbrains.kotlin.jvm` from 2.3.21 to 2.4.0
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.21...v2.4.0)
    
    Updates `org.jetbrains.kotlin.plugin.serialization` from 2.3.21 to 2.4.0
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.21...v2.4.0)
    
    Updates `org.jetbrains.kotlin.plugin.spring` from 2.3.21 to 2.4.0
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.21...v2.4.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.kotlin.jvm
      dependency-version: 2.4.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: org.jetbrains.kotlin.plugin.serialization
      dependency-version: 2.4.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: org.jetbrains.kotlin.plugin.spring
      dependency-version: 2.4.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: org.jetbrains.kotlin:kotlin-gradle-plugin
      dependency-version: 2.4.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: org.jetbrains.kotlin:kotlin-gradle-plugin-api
      dependency-version: 2.4.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump app.softwork.validation from 0.0.16 to 0.0.17
    
    Bumps [app.softwork.validation](https://github.com/hfhbd/validation) from 0.0.16 to 0.0.17.
    - [Release notes](https://github.com/hfhbd/validation/releases)
    - [Commits](https://github.com/hfhbd/validation/compare/v0.0.16...v0.0.17)
    
    ---
    updated-dependencies:
    - dependency-name: app.softwork.validation
      dependency-version: 0.0.17
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump app.softwork.serviceloader-compiler from 0.0.22 to 0.0.24
    
    Bumps [app.softwork.serviceloader-compiler](https://github.com/hfhbd/serviceloader-gradle-plugin) from 0.0.22 to 0.0.24.
    - [Release notes](https://github.com/hfhbd/serviceloader-gradle-plugin/releases)
    - [Commits](https://github.com/hfhbd/serviceloader-gradle-plugin/compare/v0.0.22...v0.0.24)
    
    ---
    updated-dependencies:
    - dependency-name: app.softwork.serviceloader-compiler
      dependency-version: 0.0.24
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * WIP
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ gradle-feature/src/main/kotlin/io/github/hfhbd/kfx/KfxFeature.kt
@@ -0,0 +1,40 @@
+package io.github.hfhbd.kfx
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.features.annotations.BindsProjectFeature
+import org.gradle.features.binding.ProjectFeatureApplicationContext
+import org.gradle.features.binding.ProjectFeatureApplyAction
+import org.gradle.features.binding.ProjectFeatureBinding
+import org.gradle.features.binding.ProjectFeatureBindingBuilder
+import org.gradle.features.dsl.bindProjectFeature
+import org.jetbrains.kotlin.gradle.declarative.projecttypes.jvmapplication.JvmApplicationProjectType
+
+@BindsProjectFeature(KfxFeature.Binding::class)
+class KfxFeature : Plugin<Project> {
+    override fun apply(target: Project) = Unit
+
+    class Binding : ProjectFeatureBinding {
+        override fun bind(builder: ProjectFeatureBindingBuilder) {
+            builder.bindProjectFeature("kfx", MainApplyAction::class)
+                .withUnsafeDefinition()
+        }
+
+        internal abstract class MainApplyAction :
+            KfxApplyAction(),
+            ProjectFeatureApplyAction<KfxDefinition, KfxBuildModel, JvmApplicationProjectType> {
+            override fun apply(
+                context: ProjectFeatureApplicationContext,
+                definition: KfxDefinition,
+                buildModel: KfxBuildModel,
+                targetDefinition: JvmApplicationProjectType,
+            ) {
+                val parentBuildModel = context.getBuildModel(targetDefinition)
+                val compilationUnits = parentBuildModel.compilationUnits
+                val sourceDirectorySet = compilationUnits.getByName("main").sources
+
+                apply(definition, sourceDirectorySet)
+            }
+        }
+    }
+}
